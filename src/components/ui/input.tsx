0503f0c27a17bfd47016effff7334bb2f3b1d813
Salliii <silas.schulreich@gmail.com>
2025-06-05 23:25:46 +0200

    refactor: change input component text size

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -6,7 +6,7 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
 		<input
 			type={type}
 			className={cn(
-				"w-full min-w-0 h-10 flex px-3 py-2 bg-card rounded-md border text-base ring-offset-background transition-[color,box-shadow,border] placeholder:text-muted md:text-sm",
+				"w-full min-w-0 h-10 flex px-3 py-2 bg-card rounded-md border text-sm ring-offset-background transition-[color,box-shadow,border] placeholder:text-muted",
 				"focus:border-ring focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
 				"disabled:cursor-default disabled:opacity-50 disabled:pointer-events-none",
 				"aria-invalid:ring-destructive aria-invalid:border-destructive",
