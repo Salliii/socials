6e55fd7ae97e781af778532ca2f82cc6a36c2261
Salliii <silas.schulreich@gmail.com>
2025-06-05 20:23:11 +0200

    feature: add inter font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,19 @@
 import "@/styles/globals.css";
+import { cn } from "@/lib/utils";
+import { Inter } from "next/font/google";
 import React from "react";
+
+const fontInter = Inter({
+	subsets: ["latin"],
+	variable: "--font-inter",
+});
 
 function AppLayout({ children }: Readonly<{ children: React.ReactNode }>) {
 	return (
 		<html suppressHydrationWarning lang={"en"}>
-			<body>{children}</body>
+			<body className={cn(fontInter.variable, fontInter.className)}>
+				{children}
+			</body>
 		</html>
 	);
 }
