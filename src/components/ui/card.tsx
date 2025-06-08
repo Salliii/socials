96551879818274dc817640d392e7900bc1572f38
Salliii <silas.schulreich@gmail.com>
2025-06-09 00:28:32 +0200

    refactor: update card component

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -5,7 +5,32 @@ function Card({ className, ...props }: React.ComponentProps<"div">) {
 	return (
 		<div
 			className={cn(
-				"w-full p-6 space-y-4 bg-card backdrop-blur-md border rounded-xl",
+				"w-full py-6 bg-card backdrop-blur-md border rounded-xl",
+				className
+			)}
+			{...props}
+		/>
+	);
+}
+
+function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
+	return (
+		<div
+			className={cn("mb-4 px-6 flex items-center space-x-3", className)}
+			{...props}
+		/>
+	);
+}
+
+function CardContent({ className, ...props }: React.ComponentProps<"div">) {
+	return <div className={cn("px-6 space-y-4", className)} {...props} />;
+}
+
+function CardFooter({ className, ...props }: React.ComponentProps<"div">) {
+	return (
+		<div
+			className={cn(
+				"mt-4 px-6 flex items-center justify-end space-x-3",
 				className
 			)}
 			{...props}
@@ -15,11 +40,11 @@ function Card({ className, ...props }: React.ComponentProps<"div">) {
 
 function CardTitle({ className, ...props }: React.ComponentProps<"div">) {
 	return (
-		<div
+		<h4
 			className={cn("text-xl font-medium leading-none", className)}
 			{...props}
 		/>
 	);
 }
 
-export { Card, CardTitle };
+export { Card, CardHeader, CardContent, CardFooter, CardTitle };
